1bebfd9f70605b1f0f6075cb633afe5af811424c
AniketSharma-dev <aniketsharma.xo@gmail.com>
2024-10-28 19:59:32 +0530

    Animation and Style

--- app.js
+++ app.js
@@ -1,29 +1,67 @@
-gsap.to(".imageContainer", {
-  width: "100%",
-  ease: Expo.easeInOut,
-  duration: 2,
-  stagger: 2,
+var tl = gsap.timeline({
+  repeat: -1,
 });
 
-gsap.to(".imageContainer", {
-  delay: 2,
-  width: "0",
-  ease: Expo.easeInOut,
-  duration: 2,
-  stagger: 2,
-});
-
-gsap.to(".text h1", {
-  top: 0,
-  ease: Expo.easeInOut,
-  duration: 2,
-  stagger: 2,
-});
-
-gsap.to(".text h1", {
-  delay: 2,
-  top: 120,
-  ease: Expo.easeInOut,
-  duration: 2,
-  stagger: 2,
-});
+tl.to(
+  ".imageContainer",
+  {
+    width: "100%",
+    ease: Expo.easeInOut,
+    duration: 2,
+    stagger: 4,
+  },
+  "a"
+)
+  .to(
+    ".imageContainer",
+    {
+      delay: 2,
+      width: "0%",
+      ease: Expo.easeInOut,
+      duration: 2,
+      stagger: 4,
+    },
+    "a"
+  )
+  .to(
+    ".text h1",
+    {
+      top: 0,
+      ease: Expo.easeInOut,
+      duration: 2,
+      stagger: 4,
+    },
+    "a"
+  )
+  .to(
+    ".text h1",
+    {
+      delay: 2,
+      top: 100,
+      ease: Expo.easeInOut,
+      duration: 2,
+      stagger: 4,
+    },
+    "a"
+  )
+  .to(
+    ".content p",
+    {
+      top: 0,
+      ease: Expo.easeInOut,
+      duration: 2,
+      stagger: 4,
+    },
+    "a"
+  )
+  .to(
+    ".content p",
+    {
+      delay: 2,
+      top: 100,
+      ease: Expo.easeInOut,
+      duration: 2,
+      stagger: 4,
+    },
+    "a"
+  );
